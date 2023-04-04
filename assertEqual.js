adfbbc269c346b7e6e89623b8f5e68c636d67a61
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-03 18:09:26 -0600

    updates to assertEqual

--- assertEqual.js
+++ assertEqual.js
@@ -1,8 +1,8 @@
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
-    return "✅✅✅ " + "Assertion Passed: " + actual + " === " + expected;
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
   } else {
-    return "🛑🛑🛑 " + "Assertion Failed: " + actual + " !== " + expected;
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   }
 };
 console.log(assertEqual("You did it!", "You did it!"));
