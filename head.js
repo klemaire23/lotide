815456f60852de6f9e2b551b084ec0d76204a616
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-03 18:47:32 -0600

    adding head.js

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+    if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const head = function(array) {
+    return (array[0]);
+};
+
+console.log(assertEqual(head([5,6,7]), 5));
+console.log(assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello"));
+console.log(assertEqual(head([]), 2));
+console.log(assertEqual(head(["hiya!"]), "hiya"));
