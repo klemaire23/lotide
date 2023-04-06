11c1d47422971bdda38aff1efa35b9592b05b7fc
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-05 20:45:04 -0600

    adding eqObjects.js

--- eqObjects.js
+++ eqObjects.js
@@ -1,3 +1,14 @@
+const eqArrays = function(arr1, arr2) {
+  if (arr1.length !== arr2.length) return false;
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (arr1[i] !== arr2[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
 const assertEqual = function(actual, expected) {
   if (actual === expected) {
     return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
@@ -5,5 +16,35 @@ const assertEqual = function(actual, expected) {
     return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
   }
 };
-console.log(assertEqual("You did it!", "You did it!"));
-console.log(assertEqual(16, 26));
+
+const eqObjects = function(object1, object2) {
+  let clothingKeys1 = Object.keys(object1);
+  let clothingKeys2 = Object.keys(object2);
+  if (clothingKeys1.length !== clothingKeys2.length) {
+    return false;
+  }
+  for (const feature of clothingKeys1) {
+    if (Array.isArray(clothingKeys1[feature]) && Array.isArray(clothingKeys2[feature])) {
+      eqArrays(clothingKeys1, clothingKeys2);
+      return true;
+    } else if (clothingKeys1[feature] !== clothingKeys2[feature]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+
+console.log(assertEqual(eqObjects(shirtObject, anotherShirtObject), true));
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+console.log(assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false));
+
+const multiColorShirtObject = { colors: ["red", "blue"], size: "medium" };
+const anotherMultiColorShirtObject = { size: "medium", colors: ["red", "blue"] };
+console.log(assertEqual(eqObjects(multiColorShirtObject, anotherMultiColorShirtObject), true)); // => true
+
+const longSleeveMultiColorShirtObject = { size: "medium", colors: ["red", "blue"], sleeveLength: "long" };
+console.log(assertEqual(eqObjects(multiColorShirtObject, longSleeveMultiColorShirtObject), false));
--- findKeyByValue.js
+++ findKeyByValue.js
@@ -7,10 +7,10 @@ const assertEqual = function(actual, expected) {
 };
 
 const findKeyByValue = function(collection, nameOfShow) {
-  let genres = Object.keys(collection);
-  for (const typeOfShow of genres) {
-    if (collection[typeOfShow] === nameOfShow) {
-      return typeOfShow;
+  let genres = Object.keys(collection); // pulls all of the types of shows from the collection of TV shows
+  for (const typeOfShow of genres) { // will loop through the types of shows held in our genre variable (has all of the keys in that variable)
+    if (collection[typeOfShow] === nameOfShow) { // if the typeOfShow (genre) of the collection matches the associated name of the show (of that genre)
+      return typeOfShow; // return the genre
     }
   }
 };
