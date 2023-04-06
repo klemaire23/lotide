5d4b587b87c4e76a65ab63233b14b4117fd5a575
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-05 19:43:59 -0600

    adding findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,25 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return `✅✅✅ Assertion Passed: ${actual} === ${expected}`;
+  } else {
+    return `🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`;
+  }
+};
+
+const findKeyByValue = function(collection, nameOfShow) {
+  let genres = Object.keys(collection);
+  for (const typeOfShow of genres) {
+     if (collection[typeOfShow] === nameOfShow) {
+      return typeOfShow;
+     }
+}
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama"));
+console.log(assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined));
