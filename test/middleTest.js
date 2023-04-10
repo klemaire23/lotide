07b0add68bb8af58a23c33a89a26cf8033a57ae4
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-10 13:08:53 -0600

    removing node_modules/ from the repo; also updating tailTest.js and middleTest.js with Mocha and Chai. Have also included a .gitignore file to prevent node_modules/ from being pushed to the repo.

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,7 +1,26 @@
-const assertArraysEqual = require('../assertArraysEqual');
+const assert = require('chai').assert;
 const middle = require('../middle');
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [2]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(["a", "b", "c"]), ["b"]);
+describe("#middle", () => {
+
+  it("returns [2,3] for [1, 2, 3, 4]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4]), [2,3]);
+  });
+
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), 3);
+  });
+
+  it("returns ['b'] for ['a', 'b', 'c']]", () => {
+    assert.deepEqual(middle(['a', 'b', 'c']), 'b');
+  });
+
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1,2]), []);
+  });
+  
+});
+// assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
+// assertArraysEqual(middle([1, 2, 3, 4, 5]), [2]);
+// assertArraysEqual(middle([1, 2]), []);
+// assertArraysEqual(middle(["a", "b", "c"]), ["b"]);
