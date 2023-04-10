81331b8465ed50e7a41c952b7a9279e93609731a
Kamila Lemaire <kb.lemaire@gmail.com>
2023-04-10 10:06:14 -0600

    updated tests for headTest.js using Mocha and Chai

--- middle.js
+++ middle.js
@@ -1,21 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) return false;
-
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual');
 
 const middle = function(array) {
   if (array.length <= 2) {
@@ -33,7 +16,4 @@ const middle = function(array) {
   return result;
 };
 
-assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]), [2]);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle(["a", "b", "c"]), ["b"]);
+module.exports = middle;
